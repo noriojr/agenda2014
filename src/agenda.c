5de70c8be6c1c1bd0a15da6f3f022353c641c188
a.paula.a.sanntos@gmail.com <a.paula.a.sanntos@gmail.com@e1c0c584-1f45-58fa-2c06-705680b5be1e>
2014-09-30 02:09:16 +0000

    <Toolbar-Go back>

--- src/agenda.c
+++ src/agenda.c
@@ -21,6 +21,7 @@ void fechar (GtkWidget *Objeto, gpointer Dados)
 	{
 	printf ("%s:%d Fechando Programa\n",__FILE__,__LINE__);
 	gtk_main_quit();
+
 	}
 /*funcao excluir*/
 
@@ -43,5 +44,6 @@ void excluir (GtkWidget *Objeto, gpointer Dados)
 
 
 /*fim funcao pesquisar*/
+
 
 
