851c059b103dd6e03dbe18bb047e6120faec8d8b
itoliveira1@gmail.com <itoliveira1@gmail.com@e1c0c584-1f45-58fa-2c06-705680b5be1e>
2014-09-30 01:30:01 +0000

    identaçao

--- src/agenda.c
+++ src/agenda.c
@@ -13,3 +13,22 @@ void fechar (GtkWidget *Objeto, gpointer Dados)
 	printf ("%s:%d Estou Aqui\n",__FILE__,__LINE__);
 	gtk_main_quit();
 	}
+/*funcao excluir*/
+
+
+
+/*fim funcao excluir*/
+
+
+/*funcao navegar*/
+
+
+/*fim funcao navegar*/
+
+
+/*funcao pesquisar*/
+
+
+/*fim funcao pesquisar*/
+
+
